910ec85a7e67591cac0edfadfacceeb1f1083edd
Artem <Artem@DESKTOP-JJ9O5GM>
2020-05-02 13:28:03 +0300

    hide admin panel

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,18 +2,17 @@
 
 namespace app\modules\admin\controllers;
 
-use app\models\ImageUpload;
 use app\models\Category;
+use app\models\ImageUpload;
 use app\models\Tag;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
-use yii\web\Controller;
 use yii\helpers\ArrayHelper;
+use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\UploadedFile;
-
 
 /**
  * ArticleController implements the CRUD actions for Article model.
@@ -21,7 +20,7 @@ use yii\web\UploadedFile;
 class ArticleController extends Controller
 {
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function behaviors()
     {
@@ -54,7 +53,6 @@ class ArticleController extends Controller
      * Displays a single Article model.
      * @param integer $id
      * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionView($id)
     {
@@ -72,23 +70,13 @@ class ArticleController extends Controller
     {
         $model = new Article();
 
-        //if ($_POST['Article']) 
-        //{
-            //var_dump($_POST['Article']);
-            //var_dump(Yii::$app->request->post()); die;
-            //$model->title = $_POST['Article']['title'];
-            //var_dump($model->title); die;
-            //$model->load(Yii::$app->request->post());
-            //var_dump($model->attributes); die;
-        //}
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        if ($model->load(Yii::$app->request->post()) && $model->saveArticle()) {
             return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
         }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
     }
 
     /**
@@ -96,19 +84,18 @@ class ArticleController extends Controller
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
      * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        if ($model->load(Yii::$app->request->post()) && $model->saveArticle()) {
             return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
         }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
     }
 
     /**
@@ -116,7 +103,6 @@ class ArticleController extends Controller
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
      * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
      */
     public function actionDelete($id)
     {
@@ -134,38 +120,31 @@ class ArticleController extends Controller
      */
     protected function findModel($id)
     {
-        if (($model = Article::findOne($id)) !== null) 
-        {
+        if (($model = Article::findOne($id)) !== null) {
             return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
         }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
     }
 
-    //Загрузка изображения к статье
     public function actionSetImage($id)
     {
-       // die('Page load image'); //Заголовок страницы загрузки
-       // die($id);
-       $model = new ImageUpload;
+        $model = new ImageUpload;
 
-       if (Yii::$app->request->isPost)
-       {
-           //die('Выбрали файл'); //для тестирования
-           $article = $this->findModel($id);
-           //var_dump($article->title);die;
-           $file = UploadedFile::getInstance($model, 'image');
-           
-           //var_dump(strtolower(md5(uniqid($file -> baseName))) .'.'. $file->extension);die;
+        if (Yii::$app->request->isPost)
+        {
+            $article = $this->findModel($id);
+            $file = UploadedFile::getInstance($model, 'image');
 
-           $article->saveImage($model->uploadFile($file, $article->image));
-       }
+            if($article->saveImage($model->uploadFile($file, $article->image)))
+            {
+                return $this->redirect(['view', 'id'=>$article->id]);
+            }
+        }
 
-       return $this->render('image', ['model'=>$model]);
-
+        return $this->render('image', ['model'=>$model]);
     }
 
-    //Выбор категории
     public function actionSetCategory($id)
     {
         $article = $this->findModel($id);
@@ -200,7 +179,7 @@ class ArticleController extends Controller
             $article->saveTags($tags);
             return $this->redirect(['view', 'id'=>$article->id]);
         }
-        
+
         return $this->render('tags', [
             'selectedTags'=>$selectedTags,
             'tags'=>$tags
